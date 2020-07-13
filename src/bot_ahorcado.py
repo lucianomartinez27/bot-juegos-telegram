833c746049665b068f94915c7b6e448de15c81b9
Luciano Martínez <lucianomartinez27@gmail.com>
2020-07-13 17:56:48 -0300

    se agregó json para guardar datos de usuario

--- src/bot_ahorcado.py
+++ src/bot_ahorcado.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+import json
 from bot_telegram import BotTelegram
 from ahorcado.funciones import plantilla_ahorcado
 
@@ -10,7 +11,11 @@ class BotTelegramAhorcado(BotTelegram):
     def __init__(self, nombre, token):
         BotTelegram.__init__(self, nombre, token)
         self.lista_palabras = "escopeta mandarina vasija perro zanahoria manzana computadora".upper().split()
-        self.datos_usuarios = {}
+        try:
+            datafile = open('data.json', 'r')
+            self.datos_usuarios = json.load(datafile)
+        except FileNotFoundError:
+            self.datos_usuarios = {}
 
     def start(self, update, context):
         bot = context.bot
@@ -19,29 +24,33 @@ class BotTelegramAhorcado(BotTelegram):
         self.enviar_mensaje(bot, usuario, "Hola {}, bienvenido. Ingresa /jugar para comenzar.".format(nombre))
 
     def jugar(self, update, context):
-        usuario = update.message.chat_id
+        id_usuario = update.message.chat_id
         bot = context.bot
-        self.datos_usuarios[usuario] = {}
-        palabra = self.datos_usuarios[usuario]['palabra'] = random.choice(self.lista_palabras)
-        lista_errores = self.datos_usuarios[usuario]['errores'] = []
-        lista_aciertos = self.datos_usuarios[usuario]['adivinadas'] = []
-        self.enviar_mensaje(bot, usuario, "Ingrese una letra como mensaje para comenzar a jugar:")
-        self.enviar_mensaje(bot, usuario, plantilla_ahorcado(lista_errores, lista_aciertos, palabra))
+        print(update)
+        # id_usuario se convierte en string porque las claves json deben ser de ese tipo
+        self.datos_usuarios[str(id_usuario)] = {}
+        palabra = self.datos_usuarios[str(id_usuario)]['palabra'] = random.choice(self.lista_palabras)
+        lista_errores = self.datos_usuarios[str(id_usuario)]['errores'] = []
+        lista_aciertos = self.datos_usuarios[str(id_usuario)]['adivinadas'] = []
+        with open('data.json', 'w') as datafile:
+            datafile.write(json.dumps(self.datos_usuarios))
+        self.enviar_mensaje(bot, id_usuario, "Ingrese una letra como mensaje para comenzar a jugar:")
+        self.enviar_mensaje(bot, id_usuario, plantilla_ahorcado(lista_errores, lista_aciertos, palabra))
 
     def elegir_letra(self, update, context):
         letra = update.message.text.upper()
         bot = context.bot
         usuario = update.message.chat_id
         nombre = update.message.chat.first_name
-        if not self.datos_usuarios.get(usuario, None):  # Comprueba si hay datos del usuario.
+        if not self.datos_usuarios.get(str(usuario), None):  # Comprueba si hay datos del usuario.
             self.enviar_mensaje(bot, usuario, "Hola {}, ingresa /jugar para comenzar.".format(nombre))
         else:
-            if len(letra) > 1 or letra.isnumeric():
+            if len(letra) > 1 or not letra.isalpha():
                 self.enviar_mensaje(bot, usuario, "POR FAVOR, INGRESA UNA LETRA.")
             else:
-                lista_errores = self.datos_usuarios[usuario]['errores']
-                palabra = self.datos_usuarios[usuario]['palabra']
-                lista_aciertos = self.datos_usuarios[usuario]['adivinadas']
+                lista_errores = self.datos_usuarios[str(usuario)]['errores']
+                palabra = self.datos_usuarios[str(usuario)]['palabra']
+                lista_aciertos = self.datos_usuarios[str(usuario)]['adivinadas']
                 if letra in lista_aciertos or letra in lista_errores:
                     self.enviar_mensaje(bot, usuario, 'YA HAS ELEGIDO ESA LETRA.')
                 elif letra in palabra:
@@ -50,12 +59,15 @@ class BotTelegramAhorcado(BotTelegram):
                     lista_errores.append(letra)
                 self.enviar_mensaje(bot, usuario, plantilla_ahorcado(lista_errores, lista_aciertos, palabra))
                 if len(lista_errores) == 6:
-                    del self.datos_usuarios[usuario]
+                    del self.datos_usuarios[str(usuario)]
                     self.enviar_mensaje(bot, usuario, "HAS PERIDO")
                     self.enviar_mensaje(bot, usuario, "LA PALABRA ERA: {}".format(palabra))
                     self.enviar_mensaje(bot, usuario, "Ingresa /jugar para comenzar de nuevo.".format(nombre))
                 elif len(lista_aciertos) == len(set(palabra)):
-                    del self.datos_usuarios[usuario]
+                    del self.datos_usuarios[str(usuario)]
                     self.enviar_mensaje(bot, usuario, "FELICITACIONES, HAS GANADO.")
                     self.enviar_mensaje(bot, usuario, "Ingresa /jugar para comenzar de nuevo.".format(nombre))
+            with open('data.json', 'w') as datafile:
+                datafile.write(json.dumps(self.datos_usuarios))
+
 
