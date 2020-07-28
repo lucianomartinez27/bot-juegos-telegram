fb2699075274a17f4324188963ac1a69e9613624
Luciano Martínez <lucianomartinez27@gmail.com>
2020-07-27 21:10:10 -0300

    se modoficó la estructura para que un bot tenga varios juegos

--- src/ahorcado/bot_ahorcado.py
+++ src/ahorcado/bot_ahorcado.py
@@ -4,53 +4,50 @@
 import random
 import json
 from bot_telegram import BotTelegram
-from ahorcado.funciones import plantilla_ahorcado
+from .funciones import plantilla_ahorcado
 
 
 class BotTelegramAhorcado(BotTelegram):
-    def __init__(self, nombre, token):
-        BotTelegram.__init__(self, nombre, token)
+    def __init__(self):
         self.lista_palabras = "escopeta mandarina vasija perro zanahoria manzana computadora".upper().split()
+        self.datos_usuarios = {}
         try:
-            datafile = open('data.json', 'r')
-            self.datos_usuarios = json.load(datafile)
-        except FileNotFoundError:
-            self.datos_usuarios = {}
-
-    def start(self, update, context):
-        bot = context.bot
-        usuario = update.message.chat_id
-        nombre = update.message.chat.first_name
-        self.enviar_mensaje(bot, usuario, "Hola {}, bienvenido. Ingresa /jugar para comenzar.".format(nombre))
+            with open('src/ahorcado/data.json', 'r+') as datafile:
+                self.datos_usuarios = json.load(datafile)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            with open('src/ahorcado/data.json', 'w') as datafile:
+                datafile.write(json.dumps(self.datos_usuarios))
 
     def jugar(self, update, context):
-        id_usuario = update.message.chat_id
+        if update.callback_query:
+             id_usuario = update.callback_query.message.chat_id
+        else:
+            id_usuario = update.message.chat_id
         bot = context.bot
-        print(update)
         # id_usuario se convierte en string porque las claves json deben ser de ese tipo
         self.datos_usuarios[str(id_usuario)] = {}
         palabra = self.datos_usuarios[str(id_usuario)]['palabra'] = random.choice(self.lista_palabras)
         lista_errores = self.datos_usuarios[str(id_usuario)]['errores'] = []
         lista_aciertos = self.datos_usuarios[str(id_usuario)]['adivinadas'] = []
-        with open('data.json', 'w') as datafile:
-            datafile.write(json.dumps(self.datos_usuarios))
-        self.enviar_mensaje(bot, id_usuario, "Ingrese una letra como mensaje para comenzar a jugar:")
+        self.datos_usuarios[str(id_usuario)]['gano_o_perdio'] = False 
+        with open('src/ahorcado/data.json', 'w') as datafile:
+                datafile.write(json.dumps(self.datos_usuarios))
+        self.enviar_mensaje(bot, id_usuario, "Ingrese una letra como mensaje para jugar:")
         self.enviar_mensaje(bot, id_usuario, plantilla_ahorcado(lista_errores, lista_aciertos, palabra))
 
-    def elegir_letra(self, update, context):
+    def responder_mensaje(self, update, context):
         letra = update.message.text.upper()
         bot = context.bot
         usuario = update.message.chat_id
         nombre = update.message.chat.first_name
-        if not self.datos_usuarios.get(str(usuario), None):  # Comprueba si hay datos del usuario.
-            self.enviar_mensaje(bot, usuario, "Hola {}, ingresa /jugar para comenzar.".format(nombre))
-        else:
+        lista_errores = self.datos_usuarios[str(usuario)]['errores']
+        palabra = self.datos_usuarios[str(usuario)]['palabra']
+        lista_aciertos = self.datos_usuarios[str(usuario)]['adivinadas']
+
+        if not self.datos_usuarios[str(usuario)]['gano_o_perdio']:
             if len(letra) > 1 or not letra.isalpha():
                 self.enviar_mensaje(bot, usuario, "POR FAVOR, INGRESA UNA LETRA.")
             else:
-                lista_errores = self.datos_usuarios[str(usuario)]['errores']
-                palabra = self.datos_usuarios[str(usuario)]['palabra']
-                lista_aciertos = self.datos_usuarios[str(usuario)]['adivinadas']
                 if letra in lista_aciertos or letra in lista_errores:
                     self.enviar_mensaje(bot, usuario, 'YA HAS ELEGIDO ESA LETRA.')
                 elif letra in palabra:
@@ -62,12 +59,20 @@ class BotTelegramAhorcado(BotTelegram):
                     del self.datos_usuarios[str(usuario)]
                     self.enviar_mensaje(bot, usuario, "HAS PERIDO")
                     self.enviar_mensaje(bot, usuario, "LA PALABRA ERA: {}".format(palabra))
-                    self.enviar_mensaje(bot, usuario, "Ingresa /jugar para comenzar de nuevo.".format(nombre))
+                    self.enviar_mensaje(bot, usuario, "¿Quieres jugar de nuevo? (Si o No)")
+                    self.datos_usuarios[str(usuario)]['gano_o_perdio'] = True
                 elif len(lista_aciertos) == len(set(palabra)):
-                    del self.datos_usuarios[str(usuario)]
                     self.enviar_mensaje(bot, usuario, "FELICITACIONES, HAS GANADO.")
-                    self.enviar_mensaje(bot, usuario, "Ingresa /jugar para comenzar de nuevo.".format(nombre))
-            with open('data.json', 'w') as datafile:
+                    self.enviar_mensaje(bot, usuario, "¿Quieres jugar de nuevo? (Si o No)")
+                    self.datos_usuarios[str(usuario)]['gano_o_perdio'] = True
+            with open('src/ahorcado/data.json', 'w') as datafile:
                 datafile.write(json.dumps(self.datos_usuarios))
+        else:
+            if letra.upper().startswith('S'):
+                self.jugar(update, context)
+            elif letra.upper().startswith('N'):
+                self.enviar_mensaje(bot, usuario, "Para jugar a otro juego puedes usar /juegos")
+            else:
+                self.enviar_mensaje(bot, usuario, "Por favor, ingresa sí o no.")
 
 
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from bot_ahorcado import BotTelegramAhorcado
+from bot_juegos import BotDeJuegosTelegram
 from config.keys import TOKEN_TELEGRAM
 
 if __name__ == '__main__':
-    bot_ahorcado = BotTelegramAhorcado("BotAhorcado", TOKEN_TELEGRAM)
-    bot_ahorcado.esperar_comando("start", bot_ahorcado.start)
-    bot_ahorcado.esperar_comando("jugar", bot_ahorcado.jugar)
-    bot_ahorcado.contestar_mensaje(bot_ahorcado.elegir_letra)
+    bot_juegos = BotDeJuegosTelegram("BotAhorcado", TOKEN_TELEGRAM)
+    bot_juegos.esperar_comando("start", bot_juegos.start)
+    bot_juegos.esperar_comando("juegos", bot_juegos.juegos)
+    bot_juegos.contestar_consulta(bot_juegos.seleccionar_juego)
+    bot_juegos.contestar_mensaje(bot_juegos.responder_mensaje_segun_juego)
