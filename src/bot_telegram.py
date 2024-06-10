9c33bae58e6017c8c0f6a2768b07c668e89b7783
Luciano Martínez <lucianomartinez27@gmail.com>
2024-06-10 19:10:43 -0300

    add health check

--- src/bot_telegram.py
+++ src/bot_telegram.py
@@ -11,77 +11,30 @@ import logging
 
 
 class BotTelegram:
-    """Clase base para crear instancias de un Bot de Telegram
-
-        >>> MiBot = BotTelegram(nombre, token)
-
-    """
 
     def __init__(self, nombre, token):
-        """Inicializa las variables básicas para que el bot de Telegram funcione."""
-        # loggin: Sirve para enviar un registro de las actividades.
         self.app = ApplicationBuilder().token(token).build()
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
         self.logger = logging.getLogger(nombre)
-        # Polling se pone a la espera de que se ingresen comandos
-        # Dispatcher: está al pendiente de todas las ventanas donde se encuentra el bot.
+
     def run(self):
         self.app.run_polling()
 
     async def enviar_mensaje(self, bot, id_usuario, mensaje, parse_mode=None) -> object:
-        """Función que envía un mensaje desde un bot y a un usuario en particular.
-        Parámetros:
-            bot: objeto Bot de el módulo telegram.
-                Tipo: telegram.bot
-            usuario: id de telegram del usuario.
-                Tipo: (int)
-            text: mensaje a enviar.
-                Tipo: (str)
-            parse_mode: establece como se 'parsea' el texto enviado.
-                Tipo: (str)
-                Ejm: 'Markdown'; 'HTML'.
-            """
         await bot.send_message(chat_id=id_usuario, text=mensaje, parse_mode=parse_mode)
 
-    def esperar_comando(self, comando, funcion):
-        """
-        Función que espera que se ingrese en el chat el comando y ejecuta la función que se ingresen como parámetro.
-        Parámetros:
-            comando: texto que colocará el usuario acompañado de una barra '/' en el chat de Telegram..
-                Tipo: (str)
-                Ejemplo: 'start'
-            funcion: función que se ejecutará cuando el usuario realice determinado comando.
-                Tipo: (fn)
-        """
-        self.app.add_handler(CommandHandler(comando, funcion))
+    def handle_command(self, comando, callback):
+
+        self.app.add_handler(CommandHandler(comando, callback))
           
-    def contestar_consulta(self, funcion, patron=None):
-        """Función que espera que el usuario presione un botón que se despliega en el chat de telegram y ejecuta la
-        función que se pase como parámetro.
-        Parametro:
-            funcion: función que se ejecuta al presionar un botón (InlineKeyboardButton) en el chat.
-                Tipo: (fn)"""
+    def handle_query(self, funcion, patron=None):
         self.app.add_handler(CallbackQueryHandler(funcion, pattern=patron))
-
-    def contestar_consulta_por_estado(self, funcion, estados):
-        conv_handler = ConversationHandler(
-            entry_points=[CommandHandler('start', funcion)],
-            states=estados,
-            fallbacks=[CommandHandler('start', funcion)]
-        )
-
-        self.app.add_handler(conv_handler)
     
-    def contestar_mensaje(self, funcion):
-        """ Espera cualquier cosa en el chat que no sea un comando (mensajes) y ejecuta la función que se pase como
-            parámetro.
-        Parametro:
-        funcion: función que se ejecuta al recibir un mensaje en el chat.
-            Tipo:: (fn)"""
-        mensaje_recibido = MessageHandler(filters.TEXT & (~filters.COMMAND), funcion)
+    def handle_message(self, callback):
+        mensaje_recibido = MessageHandler(filters.TEXT & (~filters.COMMAND), callback)
         self.app.add_handler(mensaje_recibido)
 
-    def contestar_inlinemode(self, comando, patron=None):
+    def handle_inline_mode(self, comando, patron=None):
         self.app.add_handler(InlineQueryHandler(comando, pattern=patron))
 
     def generar_id_usuario(self, update):
--- src/main.py
+++ src/main.py
@@ -8,12 +8,30 @@ import re
 patron = re.compile(r"[a-zA-ZáéíóúñÁÉÍÓÚÑ]+")
 patron_inline = re.compile(r"[a-zA-ZáéíóúñÁÉÍÓÚÑ]+[_]+ [1-9]")
 
+import http.server
+import socketserver
+
+class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Hello, world!')
+        else:
+            self.send_error(404)
+
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
+
 if __name__ == '__main__':
     bot_juegos = BotDeJuegosTelegram("BotAhorcado", TOKEN_TELEGRAM)
-    bot_juegos.esperar_comando("start", bot_juegos.start)
-    bot_juegos.esperar_comando("juegos", bot_juegos.mostrar_juegos)
-    bot_juegos.contestar_consulta(bot_juegos.seleccionar_juego, patron)
-    bot_juegos.contestar_consulta(bot_juegos.responder_boton_segun_juego)
-    bot_juegos.contestar_mensaje(bot_juegos.responder_mensaje_segun_juego)
-    bot_juegos.contestar_inlinemode(bot_juegos.mostrar_juegos_inline)
+    bot_juegos.handle_command("start", bot_juegos.start)
+    bot_juegos.handle_command("juegos", bot_juegos.mostrar_juegos)
+    bot_juegos.handle_query(bot_juegos.seleccionar_juego, patron)
+    bot_juegos.handle_query(bot_juegos.responder_boton_segun_juego)
+    bot_juegos.handle_message(bot_juegos.responder_mensaje_segun_juego)
+    bot_juegos.handle_inline_mode(bot_juegos.mostrar_juegos_inline)
     bot_juegos.run()
