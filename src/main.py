464388a3ecfb85868d0b0a351394c91183228cf4
Luciano Martínez <lucianomartinez27@gmail.com>
2024-06-10 18:36:47 -0300

    read keys from env

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from bot_juegos import BotDeJuegosTelegram
-from config.keys import TOKEN_TELEGRAM
+TOKEN_TELEGRAM = os.environ['TOKEN_TELEGRAM']
 import re
 patron = re.compile(r"[a-zA-ZáéíóúñÁÉÍÓÚÑ]+")
 patron_inline = re.compile(r"[a-zA-ZáéíóúñÁÉÍÓÚÑ]+[_]+ [1-9]")
@@ -11,7 +12,6 @@ if __name__ == '__main__':
     bot_juegos = BotDeJuegosTelegram("BotAhorcado", TOKEN_TELEGRAM)
     bot_juegos.esperar_comando("start", bot_juegos.start)
     bot_juegos.esperar_comando("juegos", bot_juegos.mostrar_juegos)
-    # bot_juegos.contestar_consulta(bot_juegos.responder_inline_segun_juego, patron_inline)
     bot_juegos.contestar_consulta(bot_juegos.seleccionar_juego, patron)
     bot_juegos.contestar_consulta(bot_juegos.responder_boton_segun_juego)
     bot_juegos.contestar_mensaje(bot_juegos.responder_mensaje_segun_juego)
